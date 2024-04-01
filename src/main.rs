3dab9bd0ce89fe8eb5a74c7cd1ab647d6c3c886f
Yang Yu <yy19@ualberta.ca>
2024-04-01 06:36:24 -0600

    send the command

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ mod http;
 mod server;
 
 fn main() {
-    let server = Server::new("127.0.0.1:80".to_string());
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
--- src/server.rs
+++ src/server.rs
@@ -25,9 +25,17 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {
-                    stream.read();
-                    println!("OK");
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                        }
+                    };
+                    // stream.read();
                 }
                 Err(e) => println!("Failed to establish a connection: {}", e),
             }
